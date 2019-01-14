e7043509cea6b22c2ead93a221d860221b486600
Lauro-Soares <lauro_bs8@hotmail.com>
2019-01-14 15:36:23 -0200

    Components

--- autoSeg-teste/src/app/app.module.ts
+++ autoSeg-teste/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AberturaComponent } from './abertura/abertura.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+import { ListasComponent } from './listas/listas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AberturaComponent,
+    CadastroComponent,
+    LoginComponent,
+    ListasComponent
   ],
   imports: [
     BrowserModule
